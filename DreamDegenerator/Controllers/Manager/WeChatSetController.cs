14e50de2fcd0dc39abbe20e19a4135acc4cc39bb
Ryanhanhui <wyhanhui@163.com>
2017-01-04 21:50:43 +0800

    引入layer 开始微信搭建

---
+++ DreamDegenerator/Controllers/Manager/WeChatSetController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamDegenerator.Controllers.Manager
+{
+    public class WeChatSetController : Controller
+    {
+        static string currentindex = "";
+        //
+        // GET: /WeChatSet/
+
+        public ActionResult Index(int id)
+        {
+            currentindex = id.ToString();
+            return View();
+        }
+
+    }
+}
